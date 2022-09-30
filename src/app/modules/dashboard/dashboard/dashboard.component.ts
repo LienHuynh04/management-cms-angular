6aff1a57cb910445119d449a3659ad476ba62d8c
Clara - LienHuynh <lien.huynh@gumiviet.com>
2022-09-30 17:36:16 +0700

    chart

--- src/app/modules/dashboard/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard/dashboard.component.ts
@@ -32,25 +32,26 @@ export class DashboardComponent implements OnInit {
     this.generalSummaryChart = {
       value: Object.values(this.record.get_general_summary.detail),
       label: Object.keys(this.record.get_general_summary.detail),
-      type: 'pie',
+      type: 'line',
       options: {
         responsive: true,
-        title: {
-          display: true,
-          text: 'Tổng quan',
-          position: 'bottom'
-        },
         aspectRatio: 1.5,
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }]
-        }
       },
-      color: []
-    }
+      color: [
+        {
+          backgroundColor: 'rgba(78, 115, 223, 0.05)',
+          borderColor: 'rgba(78, 115, 223, 1)',
+          pointRadius: 3,
+          pointBackgroundColor: 'rgba(78, 115, 223, 1)',
+          pointBorderColor: 'rgba(78, 115, 223, 1)',
+          pointHoverRadius: 3,
+          pointHoverBackgroundColor: 'rgba(78, 115, 223, 1)',
+          pointHoverBorderColor: 'rgba(78, 115, 223, 1)',
+          pointHitRadius: 10,
+          pointBorderWidth: 2,
+        }
+      ]
+    };
 
     this.teamChart = {
       value: Object.values(this.record.get_statistic_for_team.detail),
@@ -58,43 +59,35 @@ export class DashboardComponent implements OnInit {
       type: 'pie',
       options: {
         responsive: true,
-        title: {
-          display: true,
-          text: 'Nhóm kinh doanh',
-          position: 'bottom'
-        },
         aspectRatio: 1.5,
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }]
+        legend: {
+          display: true,
+          position: 'bottom'
         }
       },
       color: []
-    }
+    };
 
     this.careChart = {
       value: Object.values(this.record.get_statistic_number_care.detail),
       label: Object.keys(this.record.get_statistic_number_care.detail),
-      type: 'line',
+      type: 'doughnut',
       options: {
         responsive: true,
         aspectRatio: 1.5,
-        title: {
+        legend: {
           display: true,
-          text: 'Số lần chăm sóc',
           position: 'bottom'
-        },
+        }
       },
       color: [
         {
-          borderColor: '#86c7f3',
-          backgroundColor: 'transparent',
+          backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
+          hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+          hoverBorderColor: 'rgba(234, 236, 244, 1)',
         }
       ]
-    }
+    };
   }
 
   ngOnInit(): void {
