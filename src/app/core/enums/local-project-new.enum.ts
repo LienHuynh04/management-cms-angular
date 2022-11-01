d769d9546af04bc0e9e848dcbd3c4e3f839679af
LienHuynh04 <lien.huynh@gumiviet.com>
2022-11-02 00:18:56 +0700

    no message

---
+++ src/app/core/enums/local-project-new.enum.ts
@@ -0,0 +1,15 @@
+export enum ResultEnum {
+  NO_DEMAND = 'Không có nhu cầu',
+
+  CAN_NOT_INTERACT = 'Chưa tương tác được',
+
+  JOINED_PROJECT = 'Khách đã tham gia dự án',
+
+  GOOD_INTERACTION = 'Đang tương tác tốt',
+
+  HAVE_AN_APPOINTMENT = 'Khách đang hẹn gặp',
+
+  TRANSFER_ANOTHER_EMPLOYEE = 'Chuyển nhân viên khác',
+
+  MADE_PURCHASE = 'Đã mua hàng',
+}
